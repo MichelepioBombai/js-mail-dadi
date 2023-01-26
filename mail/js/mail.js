226751faf23180ed40587fd0e6db58b3c2d53e7c
MichelepioBombai <michelebombai@icloud.com>
2023-01-26 16:22:53 +0100

    add for/if email

--- mail/js/mail.js
+++ mail/js/mail.js
@@ -2,9 +2,34 @@
 // stampa un messaggio appropriato sull'esito del controllo.
 
 // Chiedi all'utente la sua email,
-const userEmail = prompt("qual è la tua email?")
+const userEmail = prompt("inserisci la tua email:")
 
 
 // controlla che sia nella lista di chi può accedere,
+const emailAutorizzate = [
+    "utente1@esempio.com",
+    "utente2@esempio.com",
+    "utente3@esempio.com",
+    "utente4@esempio.com",
+    "utente5@esempio.com",
+]
+
+let trovato = false;
+
+for(let i = 0; i < emailAutorizzate.lenght; i++){
+
+    if(userEmail == emailAutorizzate[i]){
+        console.log("accesso consentito");
+        trovato = true;
+    }
+
+    if(!trovato) {
+        console.log("accesso negato: email non autorizzata")
+    }
+
+
+
+
+}
 
 
