bf4b6cc82f835fec1110701607fead4d912b10f0
MichelepioBombai <michelebombai@icloud.com>
2023-01-26 15:37:48 +0100

    fist-commit email

---
+++ mail/js/mail.js
@@ -0,0 +1,10 @@
+// Mail
+// stampa un messaggio appropriato sull'esito del controllo.
+
+// Chiedi all'utente la sua email,
+const userEmail = prompt("qual è la tua email?")
+
+
+// controlla che sia nella lista di chi può accedere,
+
+
