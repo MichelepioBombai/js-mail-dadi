a5167e0b90ef6339cf51dba21ea59a71d15bd1af
MichelepioBombai <michelebombai@icloud.com>
2023-01-27 11:10:07 +0100

    last-commit add-form input

--- mail/js/mail.js
+++ mail/js/mail.js
@@ -10,29 +10,50 @@ const emailAutorizzate = [
 
 
 // Chiedi all'utente la sua email,
-const userEmail = prompt("inserisci la tua email:")
+
+const sendButton = document.getElementById("sendEmail")
 
 
 
-let trovato = false;
 
-// controlla che sia nella lista di chi può accedere,
-for(let i = 0; i < emailAutorizzate.length; i++){
-    
-    if(userEmail == emailAutorizzate[i]){
-
-        trovato = true
+sendButton.addEventListener(
+    "click",
+    function (){
         
+        
+        
+        const userEmail = document.getElementById("email").value
+        
+        
+        let trovato = false;
+
+        // controlla che sia nella lista di chi può accedere,
+
+
+        for(let i = 0; i < emailAutorizzate.length; i++){
+            
+            if (userEmail == emailAutorizzate[i]){
+        
+                trovato = true
+                
+            }
+            
+        }
+        
+        // stampa un messaggio appropriato sull'esito del controllo.
+        if(trovato){
+
+            document.getElementById("allowed").innerHTML = (`accesso autorizzato:Benvenuto/a ${userEmail}`)
+        
+            // console.log("accesso consentito");
+            
+        } else{
+
+            document.getElementById("notAllowed").innerHTML = ("accesso negato: email non autorizzata")
+
+            // console.log("accesso negato: email non autorizzata");
+        }
+
     }
-    
-}
-
-// stampa un messaggio appropriato sull'esito del controllo.
-if(trovato){
-
-    console.log("accesso consentito");
-    
-} else{
-    console.log("accesso negato: email non autorizzata");
-}
+)
 
