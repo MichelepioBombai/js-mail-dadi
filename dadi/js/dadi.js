9f2d7ae30b4d34571bf12eb20030692e2ea5f6c3
MichelepioBombai <michelebombai@icloud.com>
2023-01-27 10:23:51 +0100

    fix dadi

--- dadi/js/dadi.js
+++ dadi/js/dadi.js
@@ -1,39 +1,46 @@
 
 
+
+
+
+
+
+
+
 // // // // Gioco dei dadi
 
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-const UserRandomNumber = Math.floor(Math.random() * 6) + 1;
-const pcRandomNumber = Math.floor(Math.random() * 6) + 1;
+const startGameEl = document.getElementById("game-launch")
 
-const userDice = document.getElementById(userWin);
-const pcDice = document.getElementById(pcWin);
-const drawDice = document.getElementById(draw);
-
-
-
-
-
-
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
-
-    if(UserRandomNumber > pcRandomNumber){
-        const userWin = `ha vinto il giocatore`;
-        document.getElementById(`userWin`).innerHTML = userWin;
-
-    }
-    else if (UserRandomNumber < pcRandomNumber){
+startGameEl.addEventListener(
+    "click",
+    function () {
         
-        const pcWin = `ha vinto il computer`;
-        document.getElementById(`pcWin`).innerHTML = pcWin;
-
-
-    }
-    else if(UserRandomNumber == pcRandomNumber){
+        // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+        const UserRandomNumber = Math.floor(Math.random() * 6) + 1;
+        const pcRandomNumber = Math.floor(Math.random() * 6) + 1;
         
-        const draw = `Pareggio`
-        document.getElementById(`draw`).innerHTML = draw
+        
+        
+        // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+        if(UserRandomNumber > pcRandomNumber){
+            const userWin = `ha vinto il giocatore`;
+            document.getElementById(`userWin`).innerHTML = userWin;
+    
+        }
+        else if (UserRandomNumber < pcRandomNumber){
+        
+            const pcWin = `ha vinto il computer`;
+            document.getElementById(`pcWin`).innerHTML = pcWin;
+        }
+        else{
+        
+            const draw = `Pareggio`
+            document.getElementById(`draw`).innerHTML = draw
+        }
     }
+)
+
+
 
 
 
